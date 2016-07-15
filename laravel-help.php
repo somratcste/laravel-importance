7940e2358ed431cc6e7f2879bcc456ecdb77463b
somratcste <somratcste@gmail.com>
2016-07-15 21:01:30 +0600

    Form

--- laravel-help.php
+++ laravel-help.php
@@ -143,3 +143,23 @@ Request Facade :
 use Illuminate\Http\Request;
 ?>
 ====================================
+
+form :
+====================================
+<?php
+<form method="post" action="{{ route('admin.login') }}">
+	<div class="input-group">
+		<label for="name">Your Name : </label>
+		<input type="text" name="name" id="name" placeholder="Your name">
+	</div>
+	<div class="input-group">
+		<label for="password">Your E-Mail : </label>
+		<input type="password" name="password" id="password" placeholder="Your E-Mail">
+	</div>
+	
+	<button type="submit" class="btn btn-primary">Submti Quote</button>
+	<input type="hidden" name="_token" value="{{Session::token()}}">
+</form>
+
+?>
+=======================================
