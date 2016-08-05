8ae68970ea39c6335cade02f558634f5b77eb17f
somratcste <somratcste@gmail.com>
2016-08-06 01:09:39 +0600

    update image

--- laravel-help.php
+++ laravel-help.php
@@ -426,3 +426,64 @@ if(Input::hasFile('image')){
     $doctor->type = $file->getClientMimeType();
 }
 ?>
+
+update file / image : 
+==========================================
+<?php 
+if(Input::hasFile('image')){
+    File::delete('public/'.$doctor->image);
+    $file = Input::file('image');
+    $file->move(public_path(). '/',$file->getClientOriginalName());
+
+    $doctor->image = $file->getClientOriginalName();
+    $doctor->size = $file->getClientsize();
+    $doctor->type = $file->getClientMimeType();
+}
+?>
+
+nice modal : 
+===========================================
+<td><a data-toggle="modal" data-target="#details<?php echo $i; ?>" href=""><button type="button" class="btn btn-success">Details</button></a></td>
+<div class="modal" id="details<?php echo $i; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+      <h4 class="modal-title">Contact</h4>
+    </div>
+    <div class="modal-body">
+      <p>Feel free to contact us for any issues you might have with our products.</p>
+      <div class="row mb25">
+        <div class="col-xs-6">
+          <label>Name</label>
+          <input type="text" class="form-control" placeholder="Name">
+        </div>
+        <div class="col-xs-6">
+          <label>Email</label>
+          <input type="text" class="form-control" placeholder="Email">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <label>Message</label>
+          <textarea class="form-control" rows="3"></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer no-border">
+      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Send</button>
+    </div>
+  </div>
+</div>
+</div>
+====================================
+
+checked or selected : 
+====================================
+<?php 
+@foreach ($categories as $category)
+	<option value="{{ $category->id}}" {{ $post->category_id == $category->id ? 'selected'  : '' }}> {{ $category->name }}</option>
+@endforeach
+?>
+====================================
