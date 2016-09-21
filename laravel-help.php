09ad5671a465a0f121c7db22223de15db50ed781
somratcste <somratcste@gmail.com>
2016-09-21 12:32:19 +0600

    material design fixed

--- laravel-help.php
+++ laravel-help.php
@@ -515,12 +515,12 @@ $difference = $created->diffInDays();
 
 Google Material Design : 
 ====================================
-<ul id="slide-out" class="side-nav fixed">
+<ul id="slide-out" class="side-nav">
     <li><div class="userView">
-      <img class="background" src="{{ asset('img/technology.jpg') }}">
-      <a href="#!user"><img class="circle" src="{{ asset('img/somrat.jpg') }}"></a>
-      <a href="#!name"><span class="black-text name">G. M. Nazmul Hossain</span></a>
-      <a href="#!email"><span class="black-text email">eng.nazmulhossain@gmail.com</span></a>
+      <img class="background" src="images/technology.jpg">
+      <a href="#!user"><img class="circle" src="images/somrat.jpg"></a>
+      <a href="#!name"><span class="white-text name">John Doe</span></a>
+      <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
     </div></li>
     <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
     <li><a href="#!">Second Link</a></li>
@@ -529,16 +529,12 @@ Google Material Design :
     <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
   </ul>
   
-  <nav>
+   <nav>
     <div class="nav-wrapper">
-    <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
+    <a href="#" data-activates="slide-out" class="button-collapse" style="
+    display: -webkit-box;"><i class="material-icons">menu</i></a>
+    <a href="#!" class="brand-logo center">To Do List</a>
     </div>
   </nav>
-
+ 
   ==================================
